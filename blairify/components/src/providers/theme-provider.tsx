08b128d99a6ed0cd7057585225eceabdab0e4516
xolekk <olek.stroinski@gmail.com>
2025-11-23 20:59:25 +0000

    feat: remove blairify/components subtree configuration and documentation files

--- blairify/components/src/providers/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
